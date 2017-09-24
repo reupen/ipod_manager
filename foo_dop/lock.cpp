fd4b4175658cde4b6cefb26d21219f913b4a4672
Reupen Shah <reupen@users.noreply.github.com>
2017-09-24 20:28:06 +0100

    Remove main.cpp

--- foo_dop/lock.cpp
+++ foo_dop/lock.cpp
@@ -10,4 +10,6 @@ class app_close_blocker_impl_t : public app_close_blocker
 	}
 };
 
+bool in_ipod_sync::g_busy = false;
+
 service_factory_single_t<app_close_blocker_impl_t> g_app_close_blocker_impl;
