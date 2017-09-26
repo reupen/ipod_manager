701b436c1bf3007644d92260c65942085dbaa6cc
Reupen Shah <reupen@users.noreply.github.com>
2017-09-26 20:36:04 +0100

    Load iPhoneCalc.dll from component directory

--- foo_dop/iPhoneCalc.h
+++ foo_dop/iPhoneCalc.h
@@ -1,7 +1,5 @@
 #pragma once
 
-//#define IPHONECALC_WHITELIST
-
 class iPhoneCalc {
 	typedef void (__cdecl * xgen_hash_ab_Proc)(const t_uint8 * sha1, const t_uint8 * p_udid, t_uint8 * p_out);
 	typedef void (__cdecl * xgen_hash_ab_gen_Proc)(const t_uint8 * sha1, const t_uint8 * p_udid, t_uint32 udid_length, t_uint8 * p_out);
@@ -11,43 +9,21 @@ public:
 	{
 		if (m_library == NULL)
 		{
-			pfc::string8 fb2kpath;
-			if (uGetModuleFileName(NULL, fb2kpath) && GetLastError() == ERROR_SUCCESS)
+			pfc::string8 current_module_path;
+			if (uGetModuleFileName(mmh::get_current_instance(), current_module_path) && GetLastError() == ERROR_SUCCESS)
 			{
 				pfc::string8 path_iphonecalc;
-				path_iphonecalc << pfc::string_directory(fb2kpath) << "\\" << "iPhoneCalc.dll";
-				try 
+				path_iphonecalc << pfc::string_directory(current_module_path) << "\\" << "iPhoneCalc.dll";
+				m_library = LoadLibraryEx(pfc::stringcvt::string_wide_from_utf8(path_iphonecalc).get_ptr(), NULL, NULL);
+				if (m_library != NULL)
 				{
-#ifdef IPHONECALC_WHITELIST
-					file::ptr p_file;
-					abort_callback_dummy p_abort;
-					filesystem::g_open_read(p_file, path_iphonecalc, p_abort);
-					t_size p_size = pfc::downcast_guarded<t_size>(p_file->get_size_ex(p_abort));
-					pfc::array_staticsize_t<t_uint8> data(p_size);
-					p_file->read(data.get_ptr(), data.get_size(), p_abort);
-					p_file.release();
-					t_uint8 digest[mmh::hash::sha1_digestsize] = {0};
-					mmh::hash::sha1(data.get_ptr(), data.get_size(), digest);
-					
-					if (!memcmp("\x36\x41\xb5\x4e\x0e\xd9\x1c\x5b\xe8\x33\xb5\x07\x83\x8f\x99\x71\x6e\x28\x3d\xb4", digest, mmh::hash::sha1_digestsize)
-					|| !memcmp("\x55\xae\x84\xfc\x03\x2d\x8f\xc7\x04\x1d\x79\x3e\xe0\xa8\xf8\x74\xe6\x5b\x65\xc0", digest, mmh::hash::sha1_digestsize))
-#endif
+					m_xgen_hash_ab = (xgen_hash_ab_Proc)GetProcAddress(m_library, "xgen_hash_ab"); 
+					m_xgen_hash_ab_gen = (xgen_hash_ab_gen_Proc)GetProcAddress(m_library, "xgen_hash_ab_gen"); 
+					if (m_xgen_hash_ab == NULL && m_xgen_hash_ab_gen == NULL)
 					{
-						m_library = LoadLibraryEx(pfc::stringcvt::string_wide_from_utf8(path_iphonecalc).get_ptr(), NULL, NULL);
-						if (m_library != NULL)
-						{
-							m_xgen_hash_ab = (xgen_hash_ab_Proc)GetProcAddress(m_library, "xgen_hash_ab"); 
-							m_xgen_hash_ab_gen = (xgen_hash_ab_gen_Proc)GetProcAddress(m_library, "xgen_hash_ab_gen"); 
-							if (m_xgen_hash_ab == NULL && m_xgen_hash_ab_gen == NULL)
-							{
-								release();
-							}
-						}
+						release();
 					}
 				}
-				catch (pfc::exception const &)
-				{
-				};
 			}
 		}
 	}
